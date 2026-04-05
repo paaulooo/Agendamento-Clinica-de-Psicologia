dfdd082e090abab490cd2901cf3b161d59148821
paaulooo <paulorobertofer98@gmail.com>
2026-04-05 16:59:43 -0300

    PacienteModel.cs

---
+++ Models/PacienteModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Agendamento.Models
+{
+    public class Paciente
+    {
+        [JsonConstructor]
+        public Paciente(string nome, int idade, string convenio, string telefone, string email)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Idade = idade;
+            Convenio = convenio;
+            Telefone = telefone;
+            Email = email;
+        }
+
+        public Guid Id { get; init; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Convenio { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
