5a4078e8735a11d2954f287b0d410ca0a85b2c05
Diego <Diego@DESKTOP-IFRNRVD>
2026-04-05 17:46:21 -0300

    Adicionado PacienteRoutes e alterado Program

---
+++ Routes/PacienteRoutes.cs
@@ -0,0 +1,49 @@
+public static class PacienteRoutes
+{
+    public static void MapPacienteRoutes(this WebApplication app)
+    {
+        app.MapGet("/pacientes", (AppDbContext db) =>
+        {
+            return db.Pacientes.ToList();
+        });
+
+        app.MapGet("/pacientes/{id}", (int id, AppDbContext db) =>
+        {
+            var paciente = db.Pacientes.Find(id);
+            return paciente is not null ? Results.Ok(paciente) : Results.NotFound();
+        });
+
+        app.MapPost("/pacientes", (Paciente paciente, AppDbContext db) =>
+        {
+            db.Pacientes.Add(paciente);
+            db.SaveChanges();
+            return Results.Created($"/pacientes/{paciente.Id}", paciente);
+        });
+
+        app.MapPut("/pacientes/{id}", (int id, Paciente input, AppDbContext db) =>
+        {
+            var paciente = db.Pacientes.Find(id);
+            if (paciente is null) return Results.NotFound();
+
+            paciente.Nome = input.Nome;
+            paciente.Idade = input.Idade;
+            paciente.Convenio = input.Convenio;
+            paciente.Telefone = input.Telefone;
+            paciente.Email = input.Email;
+
+            db.SaveChanges();
+            return Results.Ok(paciente);
+        });
+
+        app.MapDelete("/pacientes/{id}", (int id, AppDbContext db) =>
+        {
+            var paciente = db.Pacientes.Find(id);
+            if (paciente is null) return Results.NotFound();
+
+            db.Pacientes.Remove(paciente);
+            db.SaveChanges();
+
+            return Results.NoContent();
+        });
+    }
+}
